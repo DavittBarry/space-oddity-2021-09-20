0f11a115393d2d30b0952cb6cb3a8bf059733794
Davitt Barry <63282613+DavittBarry@users.noreply.github.com>
2021-09-23 10:28:24 +0300

    0.0.2
    Menuvalikon responsiivisuutta parannettu ja tyylit laitettu.

--- js/common.js
+++ js/common.js
@@ -1,5 +1,59 @@
 var menuBtn = document.getElementById('menuBtn');
+var header = document.querySelector('header');
+var menu = document.getElementById('menu');
+var closeMenuBtn = document.getElementById('closeMenuBtn');
+var menuLinks = document.querySelectorAll('#menu a');
+var lastScrollTop = 0;
 
 menuBtn.onclick = function() {
     document.body.classList.toggle('menu-open');
 };
+
+function setHash(hash) {
+    var el = document.querySelector(hash)
+    el.id = hash + '-tmp';
+    window.location.hash = hash;
+    el.id = hash.replace('#', '');
+}
+
+
+window.onscroll = function () {
+    var currentScrollTop = document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop && currentScrollTop > header.offsetHeight) {
+        console.log('Going down...')
+        header.classList.add('hide')
+    }
+        else if (currentScrollTop) {
+            header.classList.add('condensed');
+            header.classList.remove('hide');
+    }
+
+
+    if (currentScrollTop <= header.offsetHeight) {
+        header.classList.remove('condensed');
+    }
+    lastScrollTop = currentScrollTop;
+}
+    for (var i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].onclick = function(e) {
+        e.preventDefault();
+        var sel = this.getAttribute('href');
+        var el = document.querySelector(sel);
+        el.scrollIntoView({
+            behavior: 'smooth'
+        });
+        setHash(sel);
+        menu.classList.remove('show');
+        menuBtn.classList.remove('hide');
+        setTimeout(function() {
+            header.classList.add('hide');
+        }, 1000);
+    };
+}
+
+
+
+
+
+
